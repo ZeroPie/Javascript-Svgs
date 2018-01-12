4fc344ed85e670b04a179da65aaa15ccb21e901d
Alejandro Palacios <alejandro.palacios@Alej-kun.local>
2018-01-12 15:04:55 +0100

    stuff

---
+++ replaceIndexMsFile.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+
+fs.readFile("./jsvgs/index.js", function(err, content) {
+    if (err) throw err;
+    if(content.indexOf('exports') >= 0){
+        console.log(content);
+    }
+});
+
+/*
+fs.appendFile('./jsvgs/index.js', "var indexMS = require('./indexMS');", (err) => {  
+    if (err) throw err;
+    console.log('updated!');
+});
+
+fs.appendFileSync('./jsvgs/index.js', 'sync', encoding='utf8');
+*/
