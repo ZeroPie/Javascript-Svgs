a4091900dab8bb0f4c12bf8f316c9cd6935272fa
S.F <skywardfyra@gmail.com>
2018-01-18 00:08:42 +0100

    insert mapping

--- scripts/oneModule.js
+++ scripts/oneModule.js
@@ -7,8 +7,22 @@ var readDir = require('readdir');
 var parse = require('parse-svg-path');
 var extract = require('extract-svg-path');
 var dirpath = './../svgs/test/';
-
 var svgFilepaths = readDir.readSync(dirpath , ['**.svg'], readDir.ABSOLUTE_PATHS);
+console.log(svgFilepaths);
+
+var allSvgs = [];
+var svgsAsVariablesArr = [];
+var svgsAsVariablesObj = {};
+var icons$1obj = {};
+var icons$1arr = [];
+var iconsPrefix = "prefix: 'fas'";
+
+for(var i = 0; i < svgFilepaths.length; i++){ 
+    readSVGContentIntoArray(svgFilepaths); 
+    svgson(allSvgs[i], getsvgsonConfig(), (result) => { 
+      chewJson(svgFilepaths,result);
+    })
+}
 
 function getsvgsonConfig() {
   var svgsonConfig = {
@@ -22,32 +36,12 @@ function getsvgsonConfig() {
   return svgsonConfig;
 }
 
-console.log(svgFilepaths);
-
-var allSvgs = [];
-var svgsAsJsonsArray = [];
-var svgsAsJsonsObj = {};
-var variableNames = [];
-var icons$1obj = {};
-var icons$1arr = [];
-
-for(var i = 0; i < svgFilepaths.length; i++){ 
-    createArrayOfVariableNames(svgFilepaths);
-    readSvgintoArray(svgFilepaths); 
-    svgson(allSvgs[i], getsvgsonConfig(),(result) => {
-      createIcons$1(svgFilepaths, result);
-      chewJson(result);
-     }
-    )
-}
 
 
-function chewJson(result) {
-  getviewBox(result);
-  saveSvgsInObject(result);
-  saveSvgsInArray(result);
-  buildJavascriptSvg(result);
-  
+
+function chewJson(svgFilepaths, result) {
+  createArrayOfVariableNames(svgFilepaths, result);
+  createIcons$1(svgFilepaths, result);
 }
 
 function saveSvgsInObject(result){
@@ -58,50 +52,63 @@ function saveSvgsInArray(result){
   svgsAsJsonsObj[i] = result;
 }
 
-function readSvgintoArray(svgFilepaths){
+function readSVGContentIntoArray(svgFilepaths){
   allSvgs[i] = sander.readFileSync(`${svgFilepaths[i]}`).toString('utf8');     //reads Buffer ergo to String
 }
 
 function getviewBox(result){
   var minx,miny,width,height;
-  console.log(result.attrs.viewBox);
+  //console.log(result.attrs.viewBox);
   return result.attrs.viewBox;
 }
 
 
-function buildJavascriptSvg(result){
-
-}
-
-function createArrayOfVariableNames(svgFilepaths){
+function createArrayOfVariableNames(svgFilepaths, result){
+  getviewBox(result);
+  var fileName = path.basename(svgFilepaths[i], path.extname(svgFilepaths[i])).replace(/-/g, "_");
+  var variableName = `var ${fileName} =`; 
+  var iconName = `iconName: '${fileName}'`;
+  var variableContent = `{ ${iconsPrefix} ${iconName} };`;
   //turn - into _ and remove file extension
-  variableNames[i] = path.basename(svgFilepaths[i], path.extname(svgFilepaths[i])).replace(/-/g, "_"); 
+  svgsAsVariablesArr[i] = `${variableName}${variableContent}`;
+  svgsAsVariablesObj = `${variableName} ${variableContent}`;
+
+
+
+
+  var svgObject = {
+    prefix: 'fas',
+    iconName : `iconName: '${fileName}'`,
+  }
 }
+
+
+
 
 function createIcons$1(result){
   //turn - into _ and remove file extension
   var icon = path.basename(svgFilepaths[i], path.extname(svgFilepaths[i])).replace(/-/g, "_"); 
   icons$1arr[i] = `${icon}:${icon}`;
-  icons$1obj[icon]= icon;
+  //icons$1obj[icon]= icon;
 }
 
+
 var icons$ = `var icons$ = {${icons$1arr.toString()}};`;
+var svgsAsVariablesArr = `${svgsAsVariablesArr.toString()};`;
+
 
 //replace/insert stuff in index.js file
-
 var indexfile = sander.readFileSync("testindex.js").toString().split("\n");
-    indexfile.splice(100, 0, icons$);
-    var text = indexfile.join("\n");
-    console.log(text);
-    fs.writeFileSync('indexAfter.js', text, function (err) {
-      if (err) return console.log(err);
-    });
-  console.log('readFileSync complete');
+    //indexfile.splice(100, 0, icons$);
+    indexfile.splice(100, 0, svgsAsVariablesArr);
+    var indexfileContent = indexfile.join("\n");
+    //console.log(indexfileContent);
+    sander.writeFileSync('indexAfter.js', indexfileContent);
 
 
-sander.writeFileSync( 'icons', 'icons$.js', icons$);
 
 //experiments/debugging/testing:
+//sander.writeFileSync( 'icons', 'icons$.js', icons$);
 //sander.writeFileSync( 'icons', 'icons$1arr.js' , util.inspect(icons$1arr));
 //sander.writeFileSync( 'icons', 'icons$1obj.js' , util.inspect(icons$1obj));
 //sander.writeFileSync( 'icons', 'svgsAsJsonsObj.json', util.inspect(svgsAsJsonsObj));
