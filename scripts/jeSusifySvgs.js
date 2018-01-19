228ccccb8edf940b0346bf0eaf323889052c8224
Alejandro Palacios <alejandro.palacios@Alej-kun.local>
2018-01-19 11:56:59 +0100

    alpha

--- scripts/jeSusifySvgs.js
+++ scripts/jeSusifySvgs.js
@@ -4,7 +4,6 @@ var util  = require('util');
 var path = require ('path');
 var svgson = require('svgson');
 var readDir = require('readdir');
-var parse = require('parse-svg-path');
 var extract = require('extract-svg-path');
 var extractViewbox = require('extract-svg-viewbox');
 var dirpath = './../svgs/test/';
@@ -14,16 +13,14 @@ console.log(svgFilepaths);
 
 var allSvgs = [];
 var svgsAsVariablesArr = [];
-var svgsAsVariablesObj = {};
-var icons$1obj = {};
 var icons$1arr = [];
 var iconsPrefix = "prefix: 'fas'";
 
 for(var i = 0; i < svgFilepaths.length; i++){ 
     readSVGContentIntoArray(svgFilepaths); 
-    svgson(allSvgs[i], getsvgsonConfig(), (result) => { 
-      chewJson(svgFilepaths,result);
-    })
+    svgson(allSvgs[i], getsvgsonConfig(), (svgAsJson) => { 
+      chewJson(svgFilepaths, svgAsJson);
+    });
 }
 
 function getsvgsonConfig() {
@@ -34,50 +31,40 @@ function getsvgsonConfig() {
     customAttrs: {
       foo: true,
     },
-  }
+  };
   return svgsonConfig;
 }
 
-function chewJson(svgFilepaths, result) {
-  createArrayOfVariableNames(svgFilepaths, result);
-  createIcons$1(svgFilepaths, result);
-}
-
-function saveSvgsInObject(result){
-  svgsAsJsonsArray.push(result);
-}
-
-function saveSvgsInArray(result){
-  svgsAsJsonsObj[i] = result;
+function chewJson(svgFilepaths, svgAsJson) {
+  createArrayOfVariableNames(svgFilepaths, svgAsJson);
+  createIcons$1(svgFilepaths, svgAsJson);
 }
 
 function readSVGContentIntoArray(svgFilepaths){
   allSvgs[i] = sander.readFileSync(`${svgFilepaths[i]}`).toString('utf8');     //reads Buffer ergo to String
-  //TODO: maybe dont parse as json at all!!!!: -->>
+  //TODO: with these maybe there is no  need to parse as json at all!!!!: -->>
   var viewBox = extractViewbox(allSvgs[i]);
 
 }
 
-function getviewBox(result){
+function getviewBox(svgAsJson){
   var minx,miny,width,height;
-  return result.attrs.viewBox;
+  return svgAsJson.attrs.viewBox;
 }
 
-
-function createArrayOfVariableNames(svgFilepaths, result){
-  getviewBox(result);
+function createArrayOfVariableNames(svgFilepaths, svgAsJson){
   var svgPath = extract(svgFilepaths[i]);
-  //var svgParsedPath = parse(svgPath);
   var fileName = path.basename(svgFilepaths[i], path.extname(svgFilepaths[i])).replace(/-/g, "_");
-  var variableName = `var ${fileName} =`; 
+  var variable = `var ${fileName} =`; 
   var iconName = `iconName: '${fileName}'`;
-  var originalViewBox = result.attrs.viewBox.replace(/\./g, "");
+  var originalViewBox = svgAsJson.attrs.viewBox.replace(/\./g, "");
   var viewBox = {
     minx: 0,
     miny: 0,
     width: 100,
     height: 100,
   };
+
   var normalizedPath = normalize({
     viewBox: `${viewBox.minx} ${viewBox.miny} ${viewBox.width} ${viewBox.height}`,
     path: svgPath,
@@ -87,43 +74,30 @@ function createArrayOfVariableNames(svgFilepaths, result){
   var children = '[]';
   var uniCode = "f2b9";
   var uniCodef = `${fileName.slice(1, -1)}`; //TODO n Stuff
-  var icon = `icon: [${viewBox.width} , ${viewBox.height} , ${children}, "${uniCode}", "${svgPath}"]`;
-
+  var icon = `icon: [${viewBox.width} , ${viewBox.height} , ${children}, "${uniCodef}", "${svgPath}"]`;
   var content = ` { ${iconsPrefix}, ${iconName}, ${icon}};`;
-  svgsAsVariablesArr[i] = `${variableName}${content}`;
+  svgsAsVariablesArr[i] = `${variable}${content}`;
   
 }
 
 
-function createIcons$1(result){
+function createIcons$1(svgAsJson){
   var icon = path.basename(svgFilepaths[i], path.extname(svgFilepaths[i])).replace(/-/g, "_"); 
-  icons$1arr[i] = `${icon}:${icon}`;
+  icons$1arr[i] = `${icon}: ${icon}`;
 }
 
 
-var icons$1 = `var icons$1 = {${icons$1arr.join(',\n')}}; \n`;
+var icons$1 = `var icons$1 = {\n${icons$1arr.join(',\n')}}; \n`;
 var svgsAsVariablesArr = `${svgsAsVariablesArr.join('\n')} \n`;
 
 function writeIndexFile(){
   var indexFileTemplate = "indexVirgin.js";
   var indexFileName = "indexAfter.js";
   var indexfile = sander.readFileSync(indexFileTemplate).toString().split("\n");
-  indexfile.splice(100, 0, `${svgsAsVariablesArr} \n ${icons$1}`);
+  indexfile.splice(100, 0, `${svgsAsVariablesArr}\n${icons$1}`);
   var indexfileContent = indexfile.join("\n");
   sander.writeFileSync(indexFileName, indexfileContent);
   sander.copyFile(indexFileName).to('../node_modules/@fortawesome/fontawesome-meinestadt/index.js');
 }
 
 writeIndexFile();
-
-
-
-
-
-
-//experiments/debugging/testing:
-//sander.writeFileSync( 'icons', 'icons$.js', icons$);
-//sander.writeFileSync( 'icons', 'icons$1arr.js' , util.inspect(icons$1arr));
-//sander.writeFileSync( 'icons', 'icons$1obj.js' , util.inspect(icons$1obj));
-//sander.writeFileSync( 'icons', 'svgsAsJsonsObj.json', util.inspect(svgsAsJsonsObj));
-//sander.writeFileSync( 'icons', 'svgsAsJsonsArray.json', util.inspect(svgsAsJsonsArray));
