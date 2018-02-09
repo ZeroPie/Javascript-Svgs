168d1fb0606f7cc9dee571e0faf689a4276001e7
Alejandro Palacios <alejandro.palacios@Alej-kun.local>
2018-02-09 15:42:40 +0100

    cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,14 @@
 var gulp = require('gulp');
 var webpack = require('gulp-webpack');
+var spawn = require('child_process').spawn;
+
 
 gulp.task('default', function() {
+    spawn('node', ['src/icons/generateLibrary.js'], { stdio: 'inherit' });
     return gulp.src('src/icons/index.js')
     .pipe(webpack({
         output:{
-            filename: 'jsSvgs.js',
+            filename: 'javascriptSvgs.js',
         }
     }))
     .pipe(gulp.dest('dist/'));
--- src/icons/utils/generateLibrary.js
+++
@@ -1,5 +0,0 @@
-var svgUtils = require('./generateJsSvgs');
-var dirpath = './svgs/jobs_icons';
-
-svgUtils.createIconsLibrary(dirpath);
-
