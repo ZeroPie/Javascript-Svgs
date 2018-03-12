1cdcab4e1389e05e23286598426245c52bdef759
Pie <highsierrapie@Highs-iMac.fritz.box>
2018-03-12 16:07:47 +0100

    listOfIcons

--- src/icons/utils/javascriptSvgsGenerator.js
+++ src/icons/utils/javascriptSvgsGenerator.js
@@ -110,7 +110,7 @@ function javascriptSvgsGenerator({
 	};
 
 	this.iconClassAndName = svg => {
-		let icon = `\t\t\t <div class = "fa-border"> <i class = "fas fa-${svg.name}"></i> &lt;i class = &quot;fas fa-${svg.name}&quot;&gt;&lt;/i&gt;</div>`;
+		let icon = `\t\t\t <div class = "fa-border"> <div class = "fa-border"> <i class = "fas fa-${svg.name}"></i> </div> <div class = "fa-border"> &lt;i class = &quot;fas fa-${svg.name}&quot;&gt;&lt;/i&gt; </div> </div>`;
 		return icon;
 	};
 
